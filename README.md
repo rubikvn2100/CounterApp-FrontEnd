b88da085f09f62b22d0e16a2aa64b85a3ba63844
Khoa Le <rubikvn2100@gmail.com>
2023-10-09 00:56:02 -0700

    Add "lint" and "format" commands into the script.

--- README.md
+++ README.md
@@ -10,6 +10,8 @@ The `cdk.json` file tells the CDK Toolkit how to execute your app.
 * `npm run watch`       watch for changes and compile
 * `npm run clean`       Remove compiled .js and .d.ts files from ./bin, ./lib, and ./test directories.
 * `npm run clean:all`   Removes compiled files, deletes node_modules, and reinstalls dependencies.
+* `npm run lint`        Check for code issues and formatting discrepancies.
+* `npm run format`      Automatically format code to maintain consistent style.
 * `npm run test`        perform the jest unit tests
 * `cdk deploy`          deploy this stack to your default AWS account/region
 * `cdk diff`            compare deployed stack with current state
--- test/counter_app-front_end.test.ts
+++ test/counter_app-front_end.test.ts
@@ -4,14 +4,13 @@
 
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/counter_app-front_end-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new CounterAppFrontEnd.CounterAppFrontEndStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
-
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+test("SQS Queue Created", () => {
+  //   const app = new cdk.App();
+  //     // WHEN
+  //   const stack = new CounterAppFrontEnd.CounterAppFrontEndStack(app, 'MyTestStack');
+  //     // THEN
+  //   const template = Template.fromStack(stack);
+  //   template.hasResourceProperties('AWS::SQS::Queue', {
+  //     VisibilityTimeout: 300
+  //   });
 });
