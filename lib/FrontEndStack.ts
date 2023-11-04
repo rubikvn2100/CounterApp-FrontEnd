0789bd95a9006f24e08173d62dcde96d229ea88b
Khoa Le <rubikvn2100@gmail.com>
2023-11-03 18:12:34 -0700

    Add CounterAppFrontEndPipeline for automated build and synth processes.

--- lib/FrontEndStack.ts
+++ lib/FrontEndStack.ts
@@ -1,8 +1,39 @@
 import * as cdk from "aws-cdk-lib";
+import {
+  CodePipeline,
+  CodePipelineSource,
+  ShellStep,
+  IFileSetProducer,
+} from "aws-cdk-lib/pipelines";
+import { githubSource } from "../config/config";
 import { Construct } from "constructs";
+
+function createGitHubSource(repoSuffix: string): IFileSetProducer {
+  const repoName = `${githubSource.owner}/${githubSource.repoName}${repoSuffix}`;
+
+  return CodePipelineSource.gitHub(repoName, githubSource.branch, {
+    authentication: cdk.SecretValue.secretsManager("github-access-token"),
+  });
+}
 
 export class FrontEndStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const repoSuffixes = ["", "-Config"];
+    const [sourceCode, configCode] = repoSuffixes.map(createGitHubSource);
+
+    new CodePipeline(this, "CounterAppFrontEndPipeline", {
+      pipelineName: "CounterAppFrontEndPipeline",
+      synth: new ShellStep("AppSynth", {
+        input: sourceCode,
+        additionalInputs: {
+          config: configCode,
+        },
+        commands: ["npm ci", "npm run deploy"],
+        primaryOutputDirectory: "cdk.out",
+      }),
+      crossAccountKeys: true,
+    });
   }
 }
