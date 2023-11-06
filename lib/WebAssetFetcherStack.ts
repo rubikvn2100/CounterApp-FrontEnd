c65a563d2beec509786eb239bae95b9b10eb9b5b
Khoa Le <rubikvn2100@gmail.com>
2023-11-05 16:47:10 -0800

    Utilize implicit IAM role creation for WebAssetFetcher Lambda.

--- lib/WebAssetFetcherStack.ts
+++ lib/WebAssetFetcherStack.ts
@@ -1,6 +1,5 @@
 import * as cdk from "aws-cdk-lib";
 import * as lambda from "aws-cdk-lib/aws-lambda";
-import * as iam from "aws-cdk-lib/aws-iam";
 import * as s3 from "aws-cdk-lib/aws-s3";
 import { Construct } from "constructs";
 
@@ -14,22 +13,10 @@ export class WebAssetFetcherStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: WebAssetFetcherStackProps) {
     super(scope, id, props);
 
-    const webAssetFetcherRole = new iam.Role(this, "WebAssetFetcherRole", {
-      assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com"),
-      managedPolicies: [
-        iam.ManagedPolicy.fromAwsManagedPolicyName(
-          "service-role/AWSLambdaBasicExecutionRole",
-        ),
-      ],
-    });
-
-    props.webAssetsBucket.grantRead(webAssetFetcherRole);
-
     this.webAssetFetcher = new lambda.Function(this, "WebAssetFetcher", {
       runtime: lambda.Runtime.PYTHON_3_8,
       code: lambda.Code.fromAsset("./lambda_code/web_asset_fetcher"),
       handler: "index.handler",
-      role: webAssetFetcherRole,
       environment: {
         stageName: id,
         account: props.env?.account || "unset-account",
@@ -37,5 +24,7 @@ export class WebAssetFetcherStack extends cdk.Stack {
         BUCKET_NAME: props.webAssetsBucket.bucketName,
       },
     });
+
+    props.webAssetsBucket.grantRead(this.webAssetFetcher);
   }
 }
